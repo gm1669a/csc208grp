25590161eccd0b2c93c77d8da0ed4675bd06fdb0
Matthew Park <mp3305a@student.american.edu>
2022-11-01 17:02:18 -0400

    saving

--- src/Villain.py
+++ src/Villain.py
@@ -7,6 +7,9 @@ class Villain:
         self.durability = durability
         self.power = power
         self.combat = combat
+        self.wins = 0
+        self.losses = 0
+        self.ties = 0
 
     def getStats(self):
         threeStatsSum = self.strength + self.durability + self.combat
